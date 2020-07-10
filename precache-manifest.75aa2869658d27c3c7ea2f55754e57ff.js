e8210ac0bfce691ae995c0fcc5182662762a1601
WalterTangHY <tang.hongyi@hotmail.com>
2020-07-10 11:42:35 +0800

    Updates

---
+++ precache-manifest.75aa2869658d27c3c7ea2f55754e57ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14577423842a1f983bc793b769779571",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "bf2c5be30051816dd666",
+    "url": "/test/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6abe7e5d2bd7f9859b96",
+    "url": "/test/static/js/2.8f0818a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.8f0818a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2c5be30051816dd666",
+    "url": "/test/static/js/main.ec6b6901.chunk.js"
+  },
+  {
+    "revision": "477b5be462aa25c0087e",
+    "url": "/test/static/js/runtime-main.5398350a.js"
+  }
+]);
